e2949d22ebbab7ce7b3714832176e5b7f9d56bd2
f.emans <frido@emans.nl>
2015-01-03 20:35:40 +0100

    some simple input validation

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,33 @@ router.get('/', function(req, res) {
     res.render('index');
 });
 
+router.get('/register',
+        function(req, res) {
+            var name = url.parse(req.url, true).query.name;
+            if(name==''||name.length>16){
+                throw new Error('Invalid name registered');
+            }
+            req.db.Game.find({players: {$elemMatch: {name: new RegExp(name, 'i')}}},
+                function(error, games){
+                    if(error) throw error;
+                    res.render('userpage', {name: name, games: games});
+                });
+        });
+
 router.post('/startgame', function(req, res) {
-    req.db.Game.startGame(req.body.players.split(',').concat(req.body.name), function(game){
+    var input=req.body.players.split(',');
+    var players=[];
+    for(p=input.length-1;p>=0;p--){
+        if(input[p].trim().length!=0&&players.indexOf(input[p].trim())==-1) {
+            players.push(input[p].trim());
+        }
+    }
+
+    console.log(players);
+    if(players.length==0||players.length>5){
+        throw new Error('Incorrect number of players');
+    }
+    req.db.Game.startGame(players.concat(req.body.name), function(game){
         game.save(function(err){
             if(err)console.error(err)
             else console.log('game', game._id, 'saved successfully');
@@ -18,16 +43,6 @@ router.post('/startgame', function(req, res) {
         });
     });
 });
-
-router.get('/register',
-        function(req, res) {
-            var name = url.parse(req.url, true).query.name;
-            req.db.Game.find({players: {$elemMatch: {name: new RegExp(name, 'i')}}},
-                function(error, games){
-                    if(error) throw error;
-                    res.render('userpage', {name: name, games: games});
-                });
-        });
 
 router.get('/arena',
         function(req, res){
